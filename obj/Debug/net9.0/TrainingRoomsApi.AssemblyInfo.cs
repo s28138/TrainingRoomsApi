0d68454fc2eb7844838de6b16cf2d24609f9dee9
Bartosz Sitek <sitekbartosz02@gmail.com>
2026-05-06 21:34:01 +0200

    Konfiguracja kontrolerów i Swaggera

--- obj/Debug/net9.0/TrainingRoomsApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TrainingRoomsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingRoomsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922f4b38a8c22f82761ad5885dcd8b572768967b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingRoomsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingRoomsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
